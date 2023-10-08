68cf0a22f53745cfbac586fd19d9895a3831d806
DanaApple <128404502+DanaApple@users.noreply.github.com>
2023-10-08 15:55:13 -0700

    neovim config

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- IMPORTANT KEYMAPS
+vim.g.mapleader = ' '
+
+-- REQUIRED
+require'plugins'
+require'remaps'
+
+-- CONFIG
+require'config.catppuccin'
+require'config.telescope'
---
+++ lua/plugins.lua
@@ -0,0 +1,41 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    
+    'nvim-lua/plenary.nvim',
+    
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+    },
+    
+     'nvim-treesitter/nvim-treesitter',
+
+     'tpope/vim-commentary',
+     'tpope/vim-fugitive',
+	
+     'airblade/vim-gitgutter',
+     { "jiaoshijie/undotree" },
+      --- Uncomment these if you want to manage LSP servers from neovim
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},     
+})
+
